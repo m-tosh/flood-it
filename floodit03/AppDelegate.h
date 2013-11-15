7ea7e0632747bf7fa76085381e74d2dbf315a9ba
ToshiyA <miyazakitoshiya@ToshiyAs-iMac.local>
2013-11-15 22:27:08 +0900

    Initial Commit

---
+++ floodit03/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  floodit03
+//
+//  Created by ToshiyA on 2013/11/15.
+//  Copyright (c) 2013年 ToshiyA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ floodit03/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  floodit03
+//
+//  Created by ToshiyA on 2013/11/15.
+//  Copyright (c) 2013年 ToshiyA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
